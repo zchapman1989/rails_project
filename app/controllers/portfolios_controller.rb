c4ffba32155351eb673c5818b2278681c907da24
zach <zchapman1989@gmail.com>
2019-03-10 13:16:17 -0400

    added portfolio index

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,10 @@
 class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  #def
+
+  #end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
+  resources :portfolios
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
-  resources :blogs, :portfolios
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
