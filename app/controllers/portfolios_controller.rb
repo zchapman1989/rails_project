1e7fa96396a22c05b5371f9249e38cb308df8575
zach <zchapman1989@gmail.com>
2019-03-10 13:45:10 -0400

    added portfolio new and create

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,8 +3,31 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
-  #def
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  #def show
 
   #end
 
+private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 end
